29ea86e1468d576f66b8c36e8f7f926165adbda3
fenil29 <fenilkaneria99@gmail.com>
2020-04-08 14:07:28 +0530

    refactored code in show parking lot map

--- screens/ShowParkingLotMap.js
+++ screens/ShowParkingLotMap.js
@@ -14,14 +14,13 @@ import {
   TouchableHighlight,
   TouchableOpacity,
   TouchableNativeFeedback,
-  StatusBar
+  StatusBar,
 } from "react-native";
 
 // import { Colors } from "react-native/Libraries/NewAppScreen";
 import Colors from "../constants/colors";
 import style from "../constants/style";
 import serverUrl from "../constants/apiUrl";
-
 
 import SelectLocationMaps from "./SelectLocationMaps";
 import Constants from "expo-constants";
@@ -34,71 +33,80 @@ import {
   Layout,
   TopNavigation,
   TopNavigationAction,
-  Spinner
+  Spinner,
 } from "@ui-kitten/components";
+
+import axios from "axios";
+
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default class ShowParkingLotMap extends React.Component {
   state = {
     selectLocationMapsModel: false,
-    location: false,
-    errorMessage: null,
-    parkingLotList: {},
+    location: { coords: { latitude: 32.715069, longitude: -117.15552 } },
+    errorMessage: false,
+    parkingLotList: [],
     isLoading: true,
-    permission: false
+    permission: false,
+    getUserLocationLoading: false,
   };
   componentDidMount() {
-    // fetch("http://google.com")
-    fetch(serverUrl + "/parking")
-      .then(response => response.json())
-      .then(responseJson => {
-        // console.log(responseJson);
-        // responseJson = [ { pd_loc_cood: "22.599936,72.8205" }];
-        if (responseJson.length == 0) {
-          throw new Error("empty");
-        }
-        if (!responseJson[0].latitude) {
-          throw new Error("Invalid Data");
+    this.setState({ isLoading: true });
+    axios(serverUrl + "/parking")
+      .then((response) => {
+        let responseDate = response.data;
+        this.setState({ isLoading: false });
+
+        if (!responseDate.length == 0 && !responseDate[0].latitude) {
+          this.setState({
+            errorMessage: "Something went wrong.Please try again.",
+          });
         }
         this.setState(
           {
-            parkingLotList: responseJson,
-            isLoading: false
+            parkingLotList: responseDate,
+            isLoading: false,
           },
-          function() {}
+          function () {}
         );
       })
-      .catch(error => {
-        // console.error(error);
-        console.log(error.message);
+      .catch((error) => {
+        console.log(error);
         this.setState({
-          errorMessage: error.message,
-          isLoading: false
+          errorMessage: "Something went wrong.Please try again.",
+          isLoading: false,
         });
       });
   }
-  componentWillMount() {
-    this._getLocationAsync();
-  }
-  _getLocationAsync = async () => {
+  componentWillMount() {}
+  getUserLocation = async () => {
+    this.setState({ getUserLocationLoading: true });
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== "granted") {
       this.setState({
-        permission: false
+        permission: false,
       });
+      this.setState({ getUserLocationLoading: false });
     }
     if (status == "granted") {
       this.setState({
-        permission: true
+        permission: true,
       });
     }
-
-    let location = await Location.getCurrentPositionAsync({});
-    // this.setState({ location });
-    this.setState({
-      location: { coords: { latitude: 32.715069, longitude: -117.15552 } }
-    });
+    try {
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+      this.setState({
+        // location: { coords: { latitude: 32.715069, longitude: -117.15552 } },
+        location: location,
+      });
+      this.setState({ getUserLocationLoading: false });
+    } catch (e) {
+      console.log(e);
+      this.setState({ getUserLocationLoading: false });
+    }
   };
-  BackIcon = style => <Icon {...style} name="arrow-ios-back-outline" />;
+  BackIcon = (style) => <Icon {...style} name="arrow-ios-back-outline" />;
 
   BackAction = () => (
     <TopNavigationAction
@@ -107,129 +115,94 @@ export default class ShowParkingLotMap extends React.Component {
     />
   );
   render() {
-    //   <View
-    //   style={{
-    //     flex: 1,
-    //     justifyContent: "center",
-    //     alignItems: "center",
-    //     height: "100%"
-    //   }}
-    // >
-    //   <Spinner color="blue" />
-    // <Text>Allow Location Permission to Use This Feature</Text>
-    // </View>
-
-    if (!this.state.permission) {
-      return (
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%"
-          }}
-        >
-          <Text>Allow Location Permission to Use This Feature</Text>
-        </View>
-      );
-    }
-
-    if (!this.state.location) {
-      return (
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%"
-          }}
-        >
-          <Spinner color="blue" />
-        </View>
-      );
-    } else if (this.state.isLoading) {
-      return (
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%"
-          }}
-        >
-          <Spinner color="blue" />
-        </View>
-      );
-    } else if (this.state.errorMessage == "empty") {
-      return (
-        <View style={styles.container}>
-          <MapView
-            style={styles.mapStyle}
-            initialRegion={{
-              latitude: this.state.location.coords.latitude,
-              longitude: this.state.location.coords.longitude,
-              latitudeDelta: 0.0922,
-              longitudeDelta: 0.0421
-            }}
-          ></MapView>
-        </View>
-      );
-    } else if (this.state.errorMessage) {
-      return (
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%"
-          }}
-        >
-          <Text>{this.state.errorMessage}</Text>
-        </View>
-      );
-    }
     return (
-      <View >
+      <View style={{ flex: 1 }}>
         <TopNavigation
           title="Parking Lot"
           alignment="center"
           leftControl={this.BackAction()}
         />
-        <MapView
-          style={styles.mapStyle}
-          initialRegion={{
-            latitude: this.state.location.coords.latitude,
-            longitude: this.state.location.coords.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={() => {
+            this.getUserLocation();
+          }}
+          style={{
+            height: 40,
+            width: 40,
+            borderWidth: 1,
+            // padding: 5,
+            borderRadius: 8,
+            backgroundColor: "#ffffffbb",
+            position: "absolute",
+            right: 15,
+            bottom: 15,
+            zIndex: 1,
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
-          {this.state.parkingLotList.map(mapInfo => (
-            <MapView.Marker
-              coordinate={{
-                latitude: Number(mapInfo.latitude),
-                longitude: Number(mapInfo.longitude)
+          {!this.state.getUserLocationLoading ? (
+            <MaterialIcons name="my-location" size={30} />
+          ) : (
+            <Spinner color="blue" />
+          )}
+        </TouchableOpacity>
+        {!this.state.isLoading ? (
+          !this.state.errorMessage ? (
+            <MapView
+              style={styles.mapStyle}
+              region={{
+                latitude: this.state.location.coords.latitude,
+                longitude: this.state.location.coords.longitude,
+                latitudeDelta: 0.1,
+                longitudeDelta: 0.1,
               }}
-              title={mapInfo.pd_loc_name}
-              onCalloutPress={()  => this.props.navigation.navigate("ParkingLotDetails",{data:mapInfo})}
-            />
-          ))}
-        </MapView>
+            >
+              {this.state.parkingLotList &&
+                this.state.parkingLotList.map((mapInfo) => (
+                  <MapView.Marker
+                    coordinate={{
+                      latitude: Number(mapInfo.latitude),
+                      longitude: Number(mapInfo.longitude),
+                    }}
+                    title={mapInfo.pd_loc_name}
+                    onCalloutPress={() =>
+                      this.props.navigation.navigate("ParkingLotDetails", {
+                        data: mapInfo,
+                      })
+                    }
+                  />
+                ))}
+            </MapView>
+          ) : (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text>{this.state.errorMessage}</Text>
+            </View>
+          )
+        ) : (
+          <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+          >
+            <Spinner color="blue" />
+          </View>
+        )}
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  },
   mapStyle: {
-    width: Dimensions.get("window").width,
+    // width: Dimensions.get("window").width,
     // height: Dimensions.get("window").height
-    height: "100%"
-  }
+    width: "100%",
+    height: "100%",
+  },
 });
